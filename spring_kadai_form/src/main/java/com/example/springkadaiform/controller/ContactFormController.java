f0d3ad8a3f9ab88a15a74b4e43992d012a1aef28
gyansan <gyansan24@outlook.jp>
2025-02-24 13:00:35 +0900

    Add files via upload

--- spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -1,31 +1,42 @@
 package com.example.springkadaiform.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.springkadaiform.form.ContactForm;
 
-import jakarta.validation.Valid;
 
 @Controller
 public class ContactFormController {
     
-    @Autowired
-    private ContactForm contactForm; 
 
     @GetMapping("/form")
     public String showForm(Model model) {
-        model.addAttribute("contactForm", new ContactForm()); 
+    	if (!model.containsAttribute("contactForm")) {
+            // ビューにフォームクラスのインスタンスを渡す
+            model.addAttribute("contactForm", new ContactForm());
+        }
         return "contactFormView";
     }
 
     @PostMapping("/confirm")
-    public String submitForm(@Valid ContactForm contactForm, BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
+    public String submitForm(RedirectAttributes redirectAttributes,@Validated ContactForm contactForm, BindingResult result, Model model
+            ) {
+        if (result.hasErrors()) {
+        	
+        	if (result.hasFieldErrors("name")) {
+                System.out.println("nameエラー: " + result.getFieldError("name").getDefaultMessage());
+            }
+        	
+            redirectAttributes.addFlashAttribute("contactForm", contactForm);
+            redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX
+                    + Conventions.getVariableName(contactForm), result);
             return "redirect:/form";
         }
 
